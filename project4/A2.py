1be8b78bc4da3912b8c5963e3e171c91d8fb8464
jfarid27 <jrab227@gmail.com>
2015-03-14 14:29:00 -0400

    project 4 additions

--- project4/A2.py
+++ project4/A2.py
@@ -4,17 +4,13 @@ x, y = 0.0, 0.0
 delta = 0.1
 n_trials = 1000000
 n_hits = 0.0
-n_samples = 0.0
-rej = 0.0
 for i in range(n_trials):
     del_x, del_y = random.uniform(-delta, delta), random.uniform(-delta, delta)
-    if abs(x + del_x) < 1.0 and abs(y + del_y) < 1.0:
-        #forcing sampling in the unit circle here
-        if x**2 + y**2 < 1.0: 
-            n_samples += 1
-            z = random.uniform(-1.0,1)
-            if x**2 + y**2 + z**2 < 1.0: n_hits += 1
+    #forcing sampling in the unit circle here
+    if abs(x+del_x)**2 + abs(y+del_y)**2 < 1.0: 
         x, y = x + del_x, y + del_y
+    z = random.uniform(-1.0,1)
+    if x**2 + y**2 + z**2 < 1.0: n_hits += 1
 
 #not using trials here due to the forcing of sampling in the unit circle
-print 2.0 * n_hits / float(n_samples)
+print 2.0 * float(n_hits) / float(n_trials)
---
+++ project4/markov_sph.py
@@ -0,0 +1,23 @@
+import random
+
+def markov_sph(n_trials=100000, delta=.75, debug=False):
+  x, y, z = 0.0, 0.0, 0.0
+  n_hits = 0
+  accepted = 0
+  for i in range(n_trials):
+      del_x = random.uniform(-delta, delta) 
+      del_y = random.uniform(-delta, delta)
+      del_z = random.uniform(-delta, delta)
+      if abs(x+del_x) < 1 and abs(y+del_y) < 1.0 and abs(z+del_z) < 1.0: 
+          accepted += 1
+          x, y, z = x + del_x, y + del_y, z + del_z
+      if x**2 + y**2 + z**2 < 1.0:
+          n_hits += 1
+  ans = 8 * float(n_hits) / n_trials
+  if debug:
+    print accepted / float(n_trials)
+    print ans
+  return ans
+
+if __name__ == "__main__":
+  markov_sph(debug=True)
